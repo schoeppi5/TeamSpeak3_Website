cb29fb915b714346ea54197ea11ed6ca48dab30b
Philipp.Schoeppner <philipp.schoeppner@fntsoftware.com>
2018-07-06 15:53:31 +0200

    Added images Restyled the frontend

--- script/js/nav.js
+++ script/js/nav.js
@@ -12,23 +12,15 @@ function resize()
 
 $(document).ready(function()
 {
-	$('#login-con').load("/include/login.html", function()
+	$.getScript("/script/js/login.js").done(function()
 	{
-		$.getScript("/script/js/login.js").done(function()
-		{
-			rememberLogin();
-		});
+		rememberLogin();
 	});
 
 	$('.drop-btn').click(function()
 	{
 		$('.hamburger').toggleClass("is-active");
 		$('.drop').slideToggle(slideSpeed);
-	});
-
-	$('#login a').click(function()
-	{
-		$('#login-con').slideToggle(slideSpeed);
 	});
 
 	$('#lang').click(function()
