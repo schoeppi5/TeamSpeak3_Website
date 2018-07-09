40b4ed4164329a5a4506bf6578998a83e45b4dbd
schoeppi <whatkarl@gmail.com>
2018-07-10 01:28:25 +0200

    pdo changes

--- script/php/database_preparation.php
+++ script/php/database_preparation.php
@@ -2,30 +2,37 @@
   include("login_config.php");
 
   try{
-    $statement = $pdo->exec("
-      CREATE DATABASE IF NOT EXISTS teamspeak;
+    $pdo->beginTransaction();
+    $statement = $pdo->exec("CREATE DATABASE IF NOT EXISTS teamspeak;");
 
-      DROP TABLE IF EXISTS admin;
-      DROP TABLE IF EXISTS users;
+    $statement = $pdo->exec("DROP TABLE IF EXISTS admin;");
+    $statement = $pdo->exec("DROP TABLE IF EXISTS users;");
 
-      CREATE TABLE IF NOT EXISTS admin (
+    $statement = $pdo->exec("CREATE TABLE IF NOT EXISTS admin (
         username VARCHAR(20) PRIMARY KEY,
         password VARCHAR(100)
-      );
+    );");
 
-      CREATE TABLE IF NOT EXISTS users (
+    $statement = $pdo->exec("CREATE TABLE IF NOT EXISTS users (
         tsuid VARCHAR(30) NOT NULL,
         uid VARCHAR(20) PRIMARY KEY
-      );
-    ");
+    );");
 
-    $statement = $pdo->prepare("
-      INSERT INTO admin (username, password)
-      VALUES ('admin', :passwd)");
+    // $statement = $pdo->prepare("
+    //   INSERT INTO admin (username, password)
+    //   VALUES ('admin', :passwd);");
+    //
+    // $passwd = password_hash("admin", PASSWORD_DEFAULT);
+    //
+    // $result = $statement->execute(array("passwd" => $passwd));
 
-    $passwd = password_hash("admin", PASSWORD_DEFAULT);
+    $pdo->commit();
 
-    $result = $statement->execute(array("passwd" => $passwd));
+    $statement = $pdo->prepare("SELECT * FROM admin;");
+
+    $statement->execute();
+
+    echo $statement->fetch();
 
     echo "Success";
   }
