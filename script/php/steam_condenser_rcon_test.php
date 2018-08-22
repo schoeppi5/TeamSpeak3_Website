9e2081c6684504e0bda850ef6b12a9292bdf6789
Philipp.Schoeppner <philipp.schoeppner@fntsoftware.com>
2018-08-22 18:05:31 +0200

    added steam-condenser

---
+++ script/php/steam_condenser_rcon_test.php
@@ -0,0 +1,21 @@
+<?php
+  include("./libs/rcon/steam-condenser/steam-condenser.php");
+
+  error_reporting(E_ERROR);
+
+  $server = new SourceServer("37.120.184.91", 27016);
+  $server->initialize();
+?>
+<pre>
+  <?php print_r($server->getServerInfo()); ?>
+</pre>
+<?php
+  try{
+    $server->rconAuth('elefanten1');
+    echo $server->rconExec("say hi");
+  }
+  catch(RCONNoAuthException $e) {
+    trigger_error('Could not authenticate with the game server.',
+    E_USER_ERROR);
+  }
+?>
